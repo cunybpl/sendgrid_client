032317ce164f79e9c48b1f25c07331477c55bad4
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-09-12 14:03:48 -0400

    Mypy fixes and template refactor

--- sendgrid_api/main.py
+++ sendgrid_api/main.py
@@ -9,22 +9,24 @@ import jinja2
 class BaseBackend(abc.ABC):
     @abc.abstractmethod
     def send_messages(
+        self,
         addresses_to: List[str],
         address_from: str,
         subject: str,
-        content: str | Dict,
-        template_name: str,
+        content: str | Dict[str, str],
+        template_name: Optional[str] = None,
     ) -> Any:
-        pass
+        ...
 
 
 class DummyBackend(BaseBackend):
     def send_messages(
+        self,
         addresses_to: List[str],
         address_from: str,
         subject: str,
-        content: str | Dict,
-        template_name: str,
+        content: str | Dict[str, str],
+        template_name: Optional[str] = None,
     ) -> Any:
         print("Dummy backend didn't do anything!")
         return len(addresses_to)
@@ -32,16 +34,14 @@ class DummyBackend(BaseBackend):
 
 class SendgridBackend(BaseBackend):
     def send_messages(
+        self,
         addresses_to: List[str],
         address_from: str,
         subject: str,
-        content: str | Dict,
+        content: str | Dict[str, str],
         template_name: Optional[str] = None,
     ) -> Any:
-        env = jinja2.Environment(
-            loader=jinja2.FileSystemLoader("./templates"),
-            autoescape="html",
-        )
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader("./templates"))
 
         if isinstance(content, str):
             mail = sgm.Mail(
@@ -51,7 +51,7 @@ class SendgridBackend(BaseBackend):
                 content=sgm.Content("text/plain", content),
             )
         else:
-            html = env.get_template(template_name).render(**content)
+            html = env.get_template(str(template_name)).render(**content)
             mail = sgm.Mail(
                 from_email=sgm.From(address_from),
                 to_emails=[sgm.To(address) for address in addresses_to],
@@ -64,17 +64,26 @@ class SendgridBackend(BaseBackend):
         return response.status_code
 
 
+_backends: Dict[str, type[BaseBackend]] = {
+    "sendgrid": SendgridBackend,
+    "dummy": DummyBackend,
+}
+
+
 # Our entrypoint
 def send_emails(
     addresses_to: List[str],
     address_from: str,
     subject: str,
-    content: str | Dict,
-    backend: BaseBackend = SendgridBackend,
-):
+    content: str | Dict[str, str],
+    backend_str: str,
+    template_name: Optional[str] = None,
+) -> Any:
+    backend = _backends[backend_str]()
     return backend.send_messages(
         address_from=address_from,
         addresses_to=addresses_to,
         subject=subject,
         content=content,
+        template_name=template_name,
     )
