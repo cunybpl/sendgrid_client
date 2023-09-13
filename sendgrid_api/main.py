644dce1dd3c11d5583dfb0c04285a99eaf0484b4
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-09-13 11:37:34 -0400

    Finish up refactoring and add tests

--- sendgrid_api/main.py
+++ sendgrid_api/main.py
@@ -1,15 +1,31 @@
 import abc
-from typing import List, Dict, Any, Optional
+from typing import List, Any
 import sendgrid
 import sendgrid.helpers.mail as sgm
-from sendgrid_api import config
+import re
+
+
+class Emails:
+    addresses: List[str]
+    _email_regex: str = r"^\S+@\S+\.\S+$"
+
+    def __init__(self, addresses: List[str]) -> None:
+        self.addresses = [self._validate(a) for a in addresses]
+
+    def _validate(self, email: str) -> str:
+        if not re.fullmatch(self._email_regex, email):
+            raise ValueError(f"Email address {email} is invalid")
+        return email
+
+    def __str__(self) -> str:
+        return " ".join(self.addresses)
 
 
 class EmailMessenger(abc.ABC):
     @abc.abstractmethod
     def send_messages(
         self,
-        addresses_to: List[str],
+        addresses_to: Emails,
         address_from: str,
         subject: str,
         content: str,
@@ -23,13 +39,17 @@ class DummyBackend(EmailMessenger):
 
     def send_messages(
         self,
-        addresses_to: List[str],
+        addresses_to: Emails,
         address_from: str,
         subject: str,
         content: str,
     ) -> Any:
-        print("Dummy backend didn't do anything!")
-        return len(addresses_to)
+        return (
+            f"Dummy backend!\nRecipients: {addresses_to}\n"
+            + f"Sender address: {address_from}\n"
+            + f"Subject: {subject}\n"
+            + f"Content: {content}"
+        )
 
 
 class SendgridBackend(EmailMessenger):
@@ -39,16 +59,31 @@ class SendgridBackend(EmailMessenger):
 
     def send_messages(
         self,
-        addresses_to: List[str],
+        addresses_to: Emails,
         address_from: str,
         subject: str,
         content: str,
     ) -> Any:
         mail = sgm.Mail(
             from_email=sgm.From(address_from),
-            to_emails=[sgm.To(address) for address in addresses_to],
+            to_emails=[sgm.To(address) for address in addresses_to.addresses],
             subject=subject,
             html_content=sgm.Content(mime_type="text/html", content=content),
         )
-        response = self.client.client.mail.send.post(request_body=mail.get())
-        return response.status_code
+        return self.client.send(mail)
+
+
+# Entrypoint
+def send_emails(
+    to_emails: Emails,
+    from_email: str,
+    backend: EmailMessenger,
+    subject: str,
+    content: str,
+) -> Any:
+    return backend.send_messages(
+        addresses_to=to_emails,
+        address_from=from_email,
+        subject=subject,
+        content=content,
+    )
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,58 @@
 import pytest
-from sendgrid_api import main
+from sendgrid_api.main import Emails, DummyBackend, SendgridBackend, send_emails
+import sendgrid
+
+
+def test_good_emails():
+    Emails(["test1@test.com", "test2@test.com"])
+
+
+def test_bad_emails():
+    with pytest.raises(ValueError):
+        Emails(["validemail@test.com", "this is not an email address"])
 
 
 def test_dummy_backend():
-    email = main.Email(
-        to_email="test@example.com",
-        from_email="test@example.com",
-        subject="Testing Subject",
-        content="<html><h1>Testing content</h2><html>",
+    to_emails = Emails(["test1@test.com", "test2@test.com"])
+    result = send_emails(
+        to_emails=to_emails,
+        from_email="test3@test.com",
+        backend=DummyBackend(),
+        subject="Testing subject",
+        content="Testing content",
     )
 
-    assert main.send_emails([email], main.DummyBackend) == 1
+    assert result == (
+        "Dummy backend!\n"
+        + "Recipients: test1@test.com test2@test.com\n"
+        + "Sender address: test3@test.com\n"
+        + "Subject: Testing subject\n"
+        + "Content: Testing content"
+    )
+
+
+def test_sendgrid_backend_401():
+    to_emails = Emails(["test1@test.com", "test2@test.com"])
+    with pytest.raises(Exception):
+        send_emails(
+            to_emails=to_emails,
+            from_email="test3@test.com",
+            backend=SendgridBackend(sendgrid_api_key="invalid api key"),
+            subject="Testing subject",
+            content="Testing content",
+        )
+
+
+def test_sendgrid_backend(mocker):
+    mocked = mocker.patch.object(sendgrid.SendGridAPIClient, "send", return_value=True)
+
+    to_emails = Emails(["test1@test.com", "test2@test.com"])
+    result = send_emails(
+        to_emails=to_emails,
+        from_email="test3@test.com",
+        backend=SendgridBackend(sendgrid_api_key="doesn't matter"),
+        subject="Testing subject",
+        content="Testing content",
+    )
+    assert result
+    assert mocked.called_once()
